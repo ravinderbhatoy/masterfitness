4dc2fb6a50c895b5ce36e460b54a21d2d6d6ffb6
Ravinder bhatoy <ravinderd.bhatoy@gmail.com>
2026-02-05 11:00:17 +0530

    create workout done

--- src/pages/addWorkout.jsx
+++ src/pages/addWorkout.jsx
@@ -3,10 +3,15 @@ import api from "../api/axios";
 
 const AddWorkout = () => {
   const handleSubmit = async () => {
+    console.log(localStorage.getItem("token"));
     try {
-      const res = await api.post("/workouts/create/", {
-        headers: { Authorization: `Token ${localStorage.getItem("token")}` },
-      });
+      const res = await api.post(
+        "workouts/create/",
+        {},
+        {
+          headers: { Authorization: `Token ${localStorage.getItem("token")}` },
+        },
+      );
     } catch (e) {
       console.log("Unable to create workout: ", e);
     }
